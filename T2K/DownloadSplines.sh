160577979891f221305821d45da9a916753387c5
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2021-07-14 07:53:41 +0200

    Add files via upload

---
+++ T2K/DownloadSplines.sh
@@ -0,0 +1,23 @@
+Path=https://nextcloud.nms.kcl.ac.uk/remote.php/dav/files/ASGReader/ASG/OA2021/ND280/MC/
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run2aMCsplines.root --output run2aMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run2wMCsplines.root --output run2wMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run3MCsplines.root --output run3MCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run4aMCsplines.root --output run4aMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run4wMCsplines.root --output run4wMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run5MCsplines.root --output run5MCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run6MCsplines.root --output run6MCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run7MCsplines.root --output run7MCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run8aMCsplines.root --output run8aMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run8wMCsplines.root --output run8wMCsplines.root
+
+curl -u ASGReader:mkND3-2k6PP-dwyM2-8coi2-rnsRR ${Path}run9MCsplines.root --output run9MCsplines.root
