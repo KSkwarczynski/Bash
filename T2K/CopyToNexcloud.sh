883c0c69874fba4dc1c854db853fdbf07cb39b81
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2021-07-22 07:37:46 +0200

    Add files via upload

---
+++ T2K/CopyToNexcloud.sh
@@ -0,0 +1,28 @@
+Path=https://nextcloud.nms.kcl.ac.uk/remote.php/dav/files/ASGReader/ASG/OA2021/ND280/MC/
+
+Password=WhatAreYouLookingFor?
+
+
+curl -u kamilskwarczynski:${Password} -T run2aMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run2wMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run3MCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run3MCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run4aMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run4wMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run5MCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run6MCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run7MCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run8aMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run8wMCsplines.root ${Path}
+
+curl -u kamilskwarczynski:${Password} -T run9MCsplines.root ${Path}
