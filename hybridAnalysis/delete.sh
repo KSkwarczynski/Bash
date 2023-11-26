c1bf106b3fe0e3195313abe274770ba400cf06b2
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2023-11-26 11:43:45 +0000

    Update delete.sh

--- hybridAnalysis/delete.sh
+++ hybridAnalysis/delete.sh
@@ -6,3 +6,8 @@ rm -r -f */*/*/*/*/nd280_elmc/
 rm -r -f */*/*/*/*/nd280_g4mc/
 rm -r -f */*/*/*/*/nd280_logs/
 rm -r -f */*/*/*/*/nd280_macs/
+rm -r -f */*/*/*/*/nd280_hitskim/
+rm -r -f */*/*/*/*/nd280_hybrid_reco/
+rm -r -f */*/*/*/*/nd280_hybrid_cali/
+
+
