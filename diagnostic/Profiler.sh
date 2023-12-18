59f5f0b1c1157b4b0817c3190f99f86fd1204727
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2023-12-18 16:21:33 +0000

    Add files via upload

---
+++ diagnostic/Profiler.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/mnt/home/kskwarczynski/T2K_OA/Valgrind/gperftools/.libs:$LD_LIBRARY_PATH
+CPUPROFILE=/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_SplineThrust/output/Profiler/ND.prof bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
+
+/mnt/home/kskwarczynski/T2K_OA/Valgrind/gperftools/src/pprof --ps --focus="runMCMC" /mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_SplineThrust/bin/ND280_MCMC /mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_SplineThrust/ND.prof.slurmid-0 > profile.ps
+
