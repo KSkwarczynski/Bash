bee13117aeb9d3f18b6c2b1b15f35f2a6ad37292
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2023-12-19 18:29:17 +0000

    Update Profiler.sh

--- diagnostic/Profiler.sh
+++ diagnostic/Profiler.sh
@@ -1,3 +1,5 @@
+-L/mnt/home/kskwarczynski/T2K_OA/Valgrind/gperftools/.libs -lprofiler
+
 export LD_LIBRARY_PATH=/mnt/home/kskwarczynski/T2K_OA/Valgrind/gperftools/.libs:$LD_LIBRARY_PATH
 CPUPROFILE=/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_SplineThrust/output/Profiler/ND.prof bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
 
