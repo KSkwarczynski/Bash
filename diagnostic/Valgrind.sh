89307b621f12b63731a45bad1518214b8fc19045
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2024-01-15 12:57:48 +0100

    Add files via upload

---
+++ diagnostic/Valgrind.sh
@@ -0,0 +1,1 @@
+/mnt/home/kskwarczynski/T2K_OA/Valgrind/valgrind_3.21.0_gcc4.9.4/build/bin/valgrind --suppressions=$ROOTSYS/etc/valgrind-root.supp --log-file="/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_DestructorMemory/ValgrindOut.txt" --leak-check=yes bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
