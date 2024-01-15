a9239be60c62536a0d2d25b0918943376ab95797
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2024-01-15 19:37:02 +0000

    Update Valgrind.sh

--- diagnostic/Valgrind.sh
+++ diagnostic/Valgrind.sh
@@ -1,3 +1,6 @@
 /mnt/home/kskwarczynski/T2K_OA/Valgrind/valgrind_3.21.0_gcc4.9.4/build/bin/valgrind --suppressions=$ROOTSYS/etc/valgrind-root.supp --log-file="/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_DestructorMemory/ValgrindOut.txt" --leak-check=yes bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
 
 /mnt/home/kskwarczynski/T2K_OA/Valgrind/valgrind_3.21.0_gcc4.9.4/build/bin/valgrind --suppressions=$ROOTSYS/etc/valgrind-root.supp --log-file="/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_DestructorMemory/ValgrindOut.txt" --leak-check=full --show-leak-kinds=all --track-origins=yes bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
+
+
+/mnt/home/kskwarczynski/T2K_OA/Valgrind/valgrind_3.21.0_gcc4.9.4/build/bin/valgrind --suppressions=$ROOTSYS/etc/valgrind-root.supp --log-file="/mnt/home/kskwarczynski/T2K_OA/MaCh3/MaCh3_DestructorMemory/CacheGrind.txt" --tool=cachegrind bin/ND280_MCMC kamilConfig/ND_MCMC.cfg
